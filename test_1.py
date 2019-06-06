dec121b8b373a179eec957b5f4caf318094fde20
KeepLearningkz <50975591+KeepLearningkz@users.noreply.github.com>
2019-06-06 14:26:33 -0700

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -37,73 +37,24 @@ def run_quickstart(uri):
         percentage_list.append(label.score)
     return name_list, percentage_list
 
-
-
-def async_detect_document(gcs_source_uri, gcs_destination_uri):
-    # Supported mime_types are: 'application/pdf' and 'image/tiff'
-    mime_type = 'application/pdf'
-
-    # How many pages should be grouped into each json output file.
-    # With a file of 5 pages
-    batch_size = 2
-
+def detect_text_uri(uri):
+    """Detects text in the file located in Google Cloud Storage or on the Web.
+    """
     client = vision.ImageAnnotatorClient()
+    image = vision.types.Image()
+    image.source.image_uri = uri
 
-    feature = vision.types.Feature(
-        type=vision.enums.Feature.Type.DOCUMENT_TEXT_DETECTION)
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
 
-    gcs_source = vision.types.GcsSource(uri=gcs_source_uri)
-    input_config = vision.types.InputConfig(
-        gcs_source=gcs_source, mime_type=mime_type)
+    for text in texts:
+        print('\n"{}"'.format(text.description))
 
-    gcs_destination = vision.types.GcsDestination(uri=gcs_destination_uri)
-    output_config = vision.types.OutputConfig(
-        gcs_destination=gcs_destination, batch_size=batch_size)
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
 
-    async_request = vision.types.AsyncAnnotateFileRequest(
-        features=[feature], input_config=input_config,
-        output_config=output_config)
-
-    operation = client.async_batch_annotate_files(
-        requests=[async_request])
-
-    print('Waiting for the operation to finish.')
-    operation.result(timeout=90)
-
-    # Once the request has completed and the output has been
-    # written to GCS, we can list all the output files.
-    storage_client = storage.Client()
-
-    match = re.match(r'gs://([^/]+)/(.+)', gcs_destination_uri)
-    bucket_name = match.group(1)
-    prefix = match.group(2)
-
-    bucket = storage_client.get_bucket(bucket_name=bucket_name)
-
-    # List objects with the given prefix.
-    blob_list = list(bucket.list_blobs(prefix=prefix))
-    print('Output files:')
-    for blob in blob_list:
-        print(blob.name)
-
-    # Process the first output file from GCS.
-    # Since we specified batch_size=2, the first response contains
-    # the first two pages of the input file.
-    output = blob_list[0]
-
-    json_string = output.download_as_string()
-    response = json_format.Parse(
-        json_string, vision.types.AnnotateFileResponse())
-
-    # The actual response for the first page of the input file.
-    first_page_response = response.responses[0]
-    annotation = first_page_response.full_text_annotation
-
-    # Here we print the full text from the first page.
-    # The response contains more information:
-    # annotation/pages/blocks/paragraphs/words/symbols
-    # including confidence scores and bounding boxes
-    print(u'Full text:\n{}'.format(annotation.text))
+        print('bounds: {}'.format(','.join(vertices)))
 
 sample1=pd.read_excel("VISION_API_G1.xlsx")
 sample2=pd.read_excel("VISION_API_G2.xlsx")
@@ -136,10 +87,10 @@ def checking_sample(sample,file_id,start,end):
     total.to_csv("sample"+file_id+".csv")
     error.to_csv("error.csv")
     
-checking_sample(sample1,"1_2000",1000,2000)
-checking_sample(sample2,"2_2000",1000,2000)
-checking_sample(sample3,"3_2000",1000,2000)
-checking_sample(sample4,"4_2000",1000,2000)
-checking_sample(sample5,"5_2000",1000,2000)
+checking_sample(sample1,"1_2000",10,20)
+#checking_sample(sample2,"2_2000",1000,2000)
+#checking_sample(sample3,"3_2000",1000,2000)
+#checking_sample(sample4,"4_2000",1000,2000)
+#checking_sample(sample5,"5_2000",1000,2000)
     
 
